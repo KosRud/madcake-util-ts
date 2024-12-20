8ca35e69632a54db70f1519a2d9e800e486cec05
MadCake <placeholder@email.com>
2024-12-20 05:50:56 +0100

    added Array.remove()

--- src/augment-Array.ts
+++ src/augment-Array.ts
@@ -3,6 +3,11 @@ import { NonEmptyArray } from './Array.ts';
 declare global {
 	interface Array<T> {
 		isNotEmpty(): this is NonEmptyArray<T>;
+		/**
+		 * Removes the first occurrence of a value in an array, returns its index or -1 if it was not present.
+		 * @param value The value to be removed.
+		 */
+		remove(value: T): void;
 	}
 }
 
@@ -10,4 +15,15 @@ if (!Array.prototype.isNotEmpty) {
 	Array.prototype.isNotEmpty = function <T>(this: T[]) {
 		return this.length > 0;
 	} as unknown as any;
+}
+
+if (!Array.prototype.remove) {
+	Array.prototype.remove = function <T>(value: T) {
+		const me = this as Array<T>;
+		const index = me.findIndex((x: T) => x == value);
+		if (index != -1) {
+			me.splice(index, 1);
+		}
+		return index;
+	};
 }
